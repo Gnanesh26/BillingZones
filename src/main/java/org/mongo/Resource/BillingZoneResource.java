eaa4967764bbe066ee9b063abc8b27da2d9d4f0f
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-14 15:48:30 +0530

    intial commit

--- src/main/java/org/mongo/Resource/BillingZoneResource.java
+++ src/main/java/org/mongo/Resource/BillingZoneResource.java
@@ -30,35 +30,25 @@ public class BillingZoneResource {
 
     @POST
     public Response createBillingZone(BillingZone newBillingZone) {
-        // Define the allowable difference for minDistance calculation
         double minDistanceDeferrable = 0.01;
 
-        // Find existing billing zones with the same name
         List<BillingZone> existingZones = BillingZone.list("name", newBillingZone.getName());
 
-        // If zones with the same name exist
         if (!existingZones.isEmpty()) {
-            // Sort existing zones by maxDistance in descending order
             existingZones.sort(Comparator.comparingDouble(BillingZone::getMaxDistance).reversed());
 
-            // Calculate the required minDistance based on the previous maxDistance
             double requiredMinDistance = existingZones.get(0).getMaxDistance() + minDistanceDeferrable;
 
-            // Check if the new minDistance adheres to the required value with a small tolerance
-            if (Math.abs(newBillingZone.getMinDistance() - requiredMinDistance) > 0.0001) {
-                // Return a BAD_REQUEST response with an error message
+            if (newBillingZone.getMinDistance() != requiredMinDistance) {
                 return Response.status(Response.Status.BAD_REQUEST)
                         .entity("Invalid distance range. Min distance should be previous max distance + 0.01")
                         .build();
             }
         }
 
-        // Set timestamps and persist the new billing zone
         newBillingZone.setUpdatedAt(new Date());
         newBillingZone.setCreatedAt(new Date());
         billingZoneRepository.persist(newBillingZone);
-
-        // Return a CREATED response with the newBillingZone entity
         return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
     }
 }
