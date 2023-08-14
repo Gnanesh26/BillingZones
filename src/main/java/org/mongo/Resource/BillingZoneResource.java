5424d2c04145e7aa31a557fa06bd2228fb677b0e
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-14 15:59:13 +0530

    intial commit

--- src/main/java/org/mongo/Resource/BillingZoneResource.java
+++ src/main/java/org/mongo/Resource/BillingZoneResource.java
@@ -30,25 +30,29 @@ public class BillingZoneResource {
 
     @POST
     public Response createBillingZone(BillingZone newBillingZone) {
+        // Defining the maximum allowable difference in minimum distance
         double minDistanceDeferrable = 0.01;
 
+        // Retrieving existing billing zones with the same name
         List<BillingZone> existingZones = BillingZone.list("name", newBillingZone.getName());
 
-        if (!existingZones.isEmpty()) {
-            existingZones.sort(Comparator.comparingDouble(BillingZone::getMaxDistance).reversed());
 
-            double requiredMinDistance = existingZones.get(0).getMaxDistance() + minDistanceDeferrable;
-
-            if (newBillingZone.getMinDistance() != requiredMinDistance) {
-                return Response.status(Response.Status.BAD_REQUEST)
-                        .entity("Invalid distance range. Min distance should be previous max distance + 0.01")
-                        .build();
-            }
+        // Checking if there are existing zones with the same name
+        if (!existingZones.isEmpty() && newBillingZone.getMinDistance() != existingZones.get(0).getMaxDistance() + minDistanceDeferrable) {
+            // Returning a BAD_REQUEST response if the minimum distance doesn't meet requirements
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid distance range. Min distance should be previous max distance + 0.01")
+                    .build();
         }
 
+        // Setting timestamps and persisting the new billing zone
         newBillingZone.setUpdatedAt(new Date());
         newBillingZone.setCreatedAt(new Date());
         billingZoneRepository.persist(newBillingZone);
-        return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
+
+        // Returning a CREATED response with the new billing zone entity
+        return Response.status(Response.Status.CREATED)
+                .entity(newBillingZone)
+                .build();
     }
 }
