950509fb0b5ea622d2e9774a8cc19c1bfaadc8b3
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-17 19:34:10 +0530

    initial commit

--- src/main/java/org/mongo/Repository/AccountRepository.java
+++ src/main/java/org/mongo/Repository/AccountRepository.java
@@ -1,7 +1,8 @@
 package org.mongo.Repository;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.mongo.Entity.Account;
-
+@ApplicationScoped
 public class AccountRepository implements PanacheMongoRepository<Account> {
 }
--- src/main/java/org/mongo/Resource/BillingZoneResource.java
+++ src/main/java/org/mongo/Resource/BillingZoneResource.java
@@ -5,8 +5,9 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.bson.types.ObjectId;
-import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.mongo.Entity.Account;
 import org.mongo.Entity.BillingZone;
+import org.mongo.Repository.AccountRepository;
 import org.mongo.Repository.BillingZoneRepository;
 
 import java.util.Comparator;
@@ -17,129 +18,71 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class BillingZoneResource {
-    //
+
     @Inject
     BillingZoneRepository billingZoneRepository;
 
-    //
-//
-    @GET
-    @Operation(summary = "Get a list of zones")
-    public List<BillingZone> getBills() {
-        return BillingZone.listAll();
-    }
-
-    //
-//
-//    @POST
-//    public Response createBillingZone(BillingZone newBillingZone) {
-//        double minDistanceDeferrable = 0.01;
-//        double tolerance = 0.0001;
-//
-//        List<BillingZone> existingZones = BillingZone.list("name", newBillingZone.getName());
-//
-//        // Sort the existing zones by maxDistance in ascending order
-//        existingZones.sort(Comparator.comparingDouble(BillingZone::getMaxDistance));
-//
-//        if (!existingZones.isEmpty()) {
-//            double previousMaxDistance = existingZones.get(existingZones.size() - 1).getMaxDistance();
-//            double requiredMinDistance = previousMaxDistance + minDistanceDeferrable;
-//
-//            double inputMinDistance = newBillingZone.getMinDistance();
-//
-//            if (Math.abs(inputMinDistance - requiredMinDistance) <= tolerance) {
-//                newBillingZone.setMinDistance(requiredMinDistance);
-//                updateAndPersistBillingZone(newBillingZone);
-//                return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
-//            } else {
-//                return Response.status(Response.Status.BAD_REQUEST)
-//                        .entity("Invalid!!! Min distance must be approximately previous maxDistance + 0.01 only")
-//                        .build();
-//            }
-//        } else {
-//            if (existingZones.isEmpty()) {
-//                newBillingZone.setUpdatedAt(new Date());
-//                newBillingZone.setCreatedAt(new Date());
-//            }
-//
-//            updateAndPersistBillingZone(newBillingZone);
-//            return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
-//        }
-//    }
-//
-//    private void updateAndPersistBillingZone(BillingZone billingZone) {
-//        billingZoneRepository.persist(billingZone);
-//    }
-//}
-//
-//
-//
-//
-//    @DELETE
-//    @Path("/{zoneId}")
-//    public Response deleteBillingZone(@PathParam("zoneId") String zoneId) {
-//        ObjectId objectId;
-//        try {
-//            objectId = new ObjectId(zoneId);
-//        } catch (IllegalArgumentException e) {
-//            return Response.status(Response.Status.BAD_REQUEST)
-//                    .entity("Invalid zone ID format.")
-//                    .build();
-//        }
-//
-//        BillingZone zoneToDelete = BillingZone.findById(objectId);
-//
-//        if (zoneToDelete == null) {
-//            return Response.status(Response.Status.NOT_FOUND)
-//                    .entity("Zone with the specified ID not found.")
-//                    .build();
-//        }
-//        zoneToDelete.delete();
-//
-//        return Response.status(Response.Status.OK)
-//                .entity("Zone configuration deleted successfully.")
-//                .build();
-//    }
-//}
+    @Inject
+    AccountRepository accountRepository;
 
 
     @POST
     public Response createBillingZone(BillingZone newBillingZone) {
+        if (newBillingZone.getAccountId() == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Account ID is required.")
+                    .build();
+        }
+
+        ObjectId accountId;
+        try {
+            accountId = new ObjectId(String.valueOf(newBillingZone.getAccountId()));
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Invalid format for Account ID.")
+                    .build();
+        }
+
+        Account account = accountRepository.findById(accountId);
+        if (account == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Account with the provided ID does not exist.")
+                    .build();
+        }
+
         double minDistanceDeferrable = 0.01;
 
-        if (newBillingZone.getMaxDistance() > newBillingZone.getMinDistance()) {
-            List<BillingZone> existingZones = BillingZone.list("name", newBillingZone.getName());
+        List<BillingZone> existingZones = BillingZone.list("accountId", accountId);
 
-            if (!existingZones.isEmpty()) {
-                existingZones.sort(Comparator.comparingDouble(BillingZone::getMaxDistance));
+        double previousMaxDistance = 0.0; // Initialize the previousMaxDistance
+        boolean isFirstEntry = true;
 
-                double previousMaxDistance = existingZones.get(existingZones.size() - 1).getMaxDistance();
-                double requiredMinDistance = previousMaxDistance + minDistanceDeferrable;
+        if (!existingZones.isEmpty()) {
+            existingZones.sort(Comparator.comparingDouble(BillingZone::getMaxDistance));
+            previousMaxDistance = existingZones.get(existingZones.size() - 1).getMaxDistance();
+            isFirstEntry = false;
+        }
 
-                if (Math.abs(newBillingZone.getMinDistance() - requiredMinDistance) <= 0.0001) {
-                    newBillingZone.setMinDistance(requiredMinDistance);
-
-                    newBillingZone.setUpdatedAt(new Date());
-                    newBillingZone.setCreatedAt(new Date());
-
-                    billingZoneRepository.persist(newBillingZone);
-                    return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
-                } else {
-                    return Response.status(Response.Status.BAD_REQUEST)
-                            .entity("Invalid!!! Min distance must be exactly previous maxDistance + 0.01 only")
-                            .build();
-                }
-            } else {
-                newBillingZone.setUpdatedAt(new Date());
-                newBillingZone.setCreatedAt(new Date());
-
-                billingZoneRepository.persist(newBillingZone);
-                return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
+        if (!isFirstEntry) {
+            double requiredMinDistance = previousMaxDistance + minDistanceDeferrable;
+            if (Math.abs(newBillingZone.getMinDistance() - requiredMinDistance) > 0.0001) {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("Invalid!!! Min distance must be exactly previous maxDistance + 0.01 only")
+                        .build();
             }
-        } else {
+        }
+
+        if (newBillingZone.getMaxDistance() <= newBillingZone.getMinDistance()) {
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Invalid!!! Max distance must be greater than min distance")
                     .build();
         }
+
+        newBillingZone.setUpdatedAt(new Date());
+        newBillingZone.setCreatedAt(new Date());
+        newBillingZone.setAccountId(accountId);
+
+        billingZoneRepository.persist(newBillingZone);
+        return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
     }
 }
