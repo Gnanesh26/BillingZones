7a8b9f7e91f401c90885825c08db7e91336c025a
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-18 15:15:26 +0530

    initial commit

--- src/main/java/org/mongo/Repository/ZipCodesRepository.java
+++ src/main/java/org/mongo/Repository/ZipCodesRepository.java
@@ -1,10 +1,9 @@
 package org.mongo.Repository;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.mongo.Entity.ZipCodes;
-
+@ApplicationScoped
 public class ZipCodesRepository implements PanacheMongoRepository<ZipCodes> {
-
-
 
 }
