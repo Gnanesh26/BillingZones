11e8e931673aa3a1694b7ae42d359bf980b2de3e
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-21 09:36:38 +0530

    initial commit

--- src/main/java/org/mongo/Resource/BillingZonesResource.java
+++ src/main/java/org/mongo/Resource/BillingZonesResource.java
@@ -9,6 +9,7 @@ import org.mongo.Entity.Account;
 import org.mongo.Entity.BillingZones;
 import org.mongo.Repository.AccountRepository;
 import org.mongo.Repository.BillingZonesRepository;
+import org.mongo.Service.BillingZoneService;
 
 import java.util.*;
 
@@ -24,79 +25,16 @@ public class BillingZonesResource {
     AccountRepository accountRepository;
 
 
+    @Inject
+    BillingZoneService billingZoneService;
+
     @POST
     public Response createBillingZone(BillingZones newBillingZone) {
-        if (newBillingZone.getAccountId() == null) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Invalid!!! Account ID is required.")
-                    .build();
-        }
-
-//        ObjectId accountId = null;
-        ObjectId accountId;
-
-        try {
-            accountId = new ObjectId(String.valueOf(newBillingZone.getAccountId()));
-        } catch (IllegalArgumentException ex) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Invalid!!! Invalid format for Account ID.")
-                    .build();
-        }
-
-
-        Account account = accountRepository.findById(accountId);
-        if (account == null) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Invalid!!! Account with the provided ID does not exist.")
-                    .build();
-        }
-
-        double minDistanceDeferrable = 0.01;
-
-        List<BillingZones> existingZones = BillingZones.list("accountId", accountId);
-//
-        double previousMaxDistance = 0.0;
-        boolean isFirst = true;
-
-        if (!existingZones.isEmpty()) {
-            existingZones.sort(Comparator.comparingDouble(BillingZones::getMaxDistance));
-            previousMaxDistance = existingZones.get(existingZones.size() - 1).getMaxDistance();
-            isFirst = false;
-        }
-
-        if (!isFirst) {
-            double requiredMinDistance = previousMaxDistance + minDistanceDeferrable;
-            if (Math.abs(newBillingZone.getMinDistance() - requiredMinDistance) > 0.01) {
-                return Response.status(Response.Status.BAD_REQUEST)
-                        .entity("Invalid!!! Min distance must be exactly previous maxDistance + 0.01 only")
-                        .build();
-            }
-        }
-
-        if (newBillingZone.getMaxDistance() <= newBillingZone.getMinDistance()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Invalid!!! Max distance must be greater than min distance")
-                    .build();
-        }
-
-//        // Check if the name is already used for the same accountId
-        boolean nameExists = existingZones.stream()
-                .anyMatch(zone -> zone.getName().equals(newBillingZone.getName()));
-
-        if (nameExists) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Invalid!!! Name must be unique for the same Account ID.")
-                    .build();
-        }
-
-        newBillingZone.setUpdatedAt(new Date());
-        newBillingZone.setCreatedAt(new Date());
-        newBillingZone.setAccountId(accountId);
-
-        billingZonesRepository.persist(newBillingZone);
-//
-        return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
+        return billingZoneService.createBillingZone(newBillingZone);
     }
+
+
+
 
 
     @DELETE
---
+++ src/main/java/org/mongo/Service/BillingZoneService.java
@@ -0,0 +1,93 @@
+package org.mongo.Service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+import org.bson.types.ObjectId;
+import org.mongo.Entity.Account;
+import org.mongo.Entity.BillingZones;
+import org.mongo.Repository.AccountRepository;
+import org.mongo.Repository.BillingZonesRepository;
+
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+@ApplicationScoped
+
+public class BillingZoneService {
+    @Inject
+    AccountRepository accountRepository;
+
+    @Inject
+    BillingZonesRepository billingZonesRepository;
+
+    public Response createBillingZone(BillingZones newBillingZone) {
+        if (newBillingZone.getAccountId() == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Account ID is required.")
+                    .build();
+        }
+
+        ObjectId accountId;
+
+        try {
+            accountId = new ObjectId(String.valueOf(newBillingZone.getAccountId()));
+        } catch (IllegalArgumentException ex) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Invalid format for Account ID.")
+                    .build();
+        }
+
+        Account account = accountRepository.findById(accountId);
+        if (account == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Account with the provided ID does not exist.")
+                    .build();
+        }
+
+        double minDistanceDeferrable = 0.01;
+
+        List<BillingZones> existingZones = BillingZones.list("accountId", accountId);
+        double previousMaxDistance = 0.0;
+        boolean isFirst = true;
+
+        if (!existingZones.isEmpty()) {
+            existingZones.sort(Comparator.comparingDouble(BillingZones::getMaxDistance));
+            previousMaxDistance = existingZones.get(existingZones.size() - 1).getMaxDistance();
+            isFirst = false;
+        }
+
+        if (!isFirst) {
+            double requiredMinDistance = previousMaxDistance + minDistanceDeferrable;
+            if (Math.abs(newBillingZone.getMinDistance() - requiredMinDistance) > 0.01) {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("Invalid!!! Min distance must be exactly previous maxDistance + 0.01 only")
+                        .build();
+            }
+        }
+
+        if (newBillingZone.getMaxDistance() <= newBillingZone.getMinDistance()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Max distance must be greater than min distance")
+                    .build();
+        }
+
+        boolean nameExists = existingZones.stream()
+                .anyMatch(zone -> zone.getName().equals(newBillingZone.getName()));
+
+        if (nameExists) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid!!! Name must be unique for the same Account ID.")
+                    .build();
+        }
+
+        newBillingZone.setUpdatedAt(new Date());
+        newBillingZone.setCreatedAt(new Date());
+        newBillingZone.setAccountId(accountId);
+
+        billingZonesRepository.persist(newBillingZone);
+
+        return Response.status(Response.Status.CREATED).entity(newBillingZone).build();
+    }
+}
+
